05b3632bec27b12927798c01616c786f9edde350
tmbenhura <tmbenhura@gmail.com>
2023-03-27 17:38:25 +0200

    Configure test case to support scribe tdd

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use AjCastro\ScribeTdd\Tests\ScribeTddSetup;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use ScribeTddSetup;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpScribeTdd();
+    }
 }
