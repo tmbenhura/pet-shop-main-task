6cdb745ea6941ddb6e5ba259f066810c0756724b
tmbenhura <tmbenhura@gmail.com>
2023-03-28 18:43:30 +0200

    Add trailing comma to php cs config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,6 +3,7 @@
 $config = new PhpCsFixer\Config();
 return $config->setRules([
         '@PSR12' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sort_algorithm' => 'length'],
     ]);
