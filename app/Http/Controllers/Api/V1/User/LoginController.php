74aaea3f181f1575c550910ce31cef00e64f1401
tmbenhura <tmbenhura@gmail.com>
2023-03-28 20:46:55 +0200

    Extract login logic to base class

--- app/Http/Controllers/Api/V1/User/LoginController.php
+++ app/Http/Controllers/Api/V1/User/LoginController.php
@@ -5,63 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api\V1\User;
 
 use App\Models\User;
-use Carbon\CarbonImmutable;
 use Illuminate\Http\Request;
-use App\Contracts\JwtTokenManager;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Hash;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Http\Controllers\BaseLoginController;
 
-class LoginController extends Controller
+class LoginController extends BaseLoginController
 {
     /**
-     * Login into api
+     * Check user existence
      */
-    public function __invoke(Request $request): JsonResponse
+    public function userExists(Request $request): bool
     {
-        if (!User::where('email', $request->email)->where('is_admin', false)->exists()) {
-            return $this->authenticationFailureResponse();
-        }
-
-        $user = User::where('email', $request->email)->first();
-
-        if (!Hash::check($request->password, $user->password)) {
-            return $this->authenticationFailureResponse();
-        }
-
-        return response()->json(
-            [
-                'data' => [
-                    'token' => $this->tokenForUser($user),
-                ],
-            ]
-        );
-    }
-
-    private function tokenForUser(User $user): string
-    {
-        return app(JwtTokenManager::class)->issueToken(
-            config('app.url'),
-            CarbonImmutable::now()->addHours(1),
-            [
-                'user_uuid' => $user->uuid,
-                'roles' => $user->roles,
-            ]
-        );
-    }
-
-    private function authenticationFailureResponse(): JsonResponse
-    {
-        return response()->json(
-            [
-                'errors' => [
-                    [
-                        'status' => '401',
-                        'title' => 'Failed to authenticate user',
-                    ],
-                ],
-            ],
-            401
-        );
+        return User::where('email', $request->email)->where('is_admin', false)->exists();
     }
 }
---
+++ app/Http/Controllers/BaseLoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Http\Request;
+use App\Contracts\JwtTokenManager;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+abstract class BaseLoginController extends Controller
+{
+    /**
+     * Check user existence
+     */
+    abstract function userExists(Request $request): bool;
+
+    /**
+     * Login into api
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        if (!$this->userExists($request)) {
+            return $this->authenticationFailureResponse();
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!Hash::check($request->password, $user->password)) {
+            return $this->authenticationFailureResponse();
+        }
+
+        return response()->json(
+            [
+                'data' => [
+                    'token' => $this->tokenForUser($user),
+                ],
+            ]
+        );
+    }
+
+    private function tokenForUser(User $user): string
+    {
+        return app(JwtTokenManager::class)->issueToken(
+            config('app.url'),
+            CarbonImmutable::now()->addHours(1),
+            [
+                'user_uuid' => $user->uuid,
+                'roles' => $user->roles,
+            ]
+        );
+    }
+
+    private function authenticationFailureResponse(): JsonResponse
+    {
+        return response()->json(
+            [
+                'errors' => [
+                    [
+                        'status' => '401',
+                        'title' => 'Failed to authenticate user',
+                    ],
+                ],
+            ],
+            401
+        );
+    }
+}
