4f5b93bf6c317b4f4de26cd74dc085ab12c22282
tmbenhura <tmbenhura@gmail.com>
2023-03-29 11:16:14 +0200

    Orders status change sends webhook notification

---
+++ tests/Feature/OrderStatusNotificationsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Seeders;
+
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Order;
+use App\Models\OrderStatus;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class OrderStatusNotificationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Orders status change sends webhook notification.
+     */
+    public function test_order_status_change_sends_webhook_notification(): void
+    {
+        config()->set('order-status-notifications.webhook_url', 'http://webhook.test');
+        Http::fake(['webhook.test' => Http::response('ok')]);
+
+        $order = Order::factory()->create();
+        $order->order_status_uuid = OrderStatus::factory()->create()->uuid;
+        $order->save();
+
+        Http::assertSent(function (Request $request) {
+            return $request->url() == 'http://webhook.test';
+        });
+    }
+}
