b134bfa7a947fe4571d89a56e3f7164db2b0faa9
tmbenhura <tmbenhura@gmail.com>
2023-03-29 12:03:01 +0200

    Add payment and order status to db seeding

--- tests/Feature/Seeders/DatabaseSeederTest.php
+++ tests/Feature/Seeders/DatabaseSeederTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\Seeders;
 
+use App\Models\Order;
+use App\Models\OrderStatus;
+use App\Models\Payment;
 use App\Models\User;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -20,8 +23,14 @@ class DatabaseSeederTest extends TestCase
 
         $adminUsers = User::where('is_admin', true)->count();
         $nonAdminUsers = User::where('is_admin', false)->count();
+        $orderStatuses = OrderStatus::count();
+        $payments = Payment::count();
+        $orders = Order::count();
 
         $this->assertEquals(1, $adminUsers);
         $this->assertEquals(10, $nonAdminUsers);
+        $this->assertEquals(5, $orderStatuses);
+        $this->assertEquals(3, $payments);
+        $this->assertGreaterThanOrEqual(50, $orders);
     }
 }
